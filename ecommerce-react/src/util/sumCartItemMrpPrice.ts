cd6cc8dd5a3df84692b7a08bea2e94abb9293fb1
Abhay-Bhandari <ab2671748@gmail.com>
2026-06-26 17:07:32 +0530

    Initial commit: Spring Boot E-Commerce Backend

--- ecommerce-react/src/util/sumCartItemMrpPrice.ts
+++
@@ -1,5 +0,0 @@
-import type { CartItem } from "../types/CartType";
-
-export const sumCartItemMrpPrice = (cartItems:CartItem[])=>{
-  return cartItems.reduce((total, item) => total + item.mrpPrice * item.quantity, 0);
-}
--- ecommerce-react/src/util/sumCartItemSellingPrice.ts
+++
@@ -1,5 +0,0 @@
-import type { CartItem } from "../types/CartType";
-
-export const sumCartItemSellingPrice = (cartItems:CartItem[])=>{
-  return cartItems.reduce((total, item) => total + item.sellingPrice * item.quantity, 0);
-}
